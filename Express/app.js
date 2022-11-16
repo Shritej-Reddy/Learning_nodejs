701e07d94e386d00633d12fdb5e4c29690bd799d
Shritej-Reddy <tejureddy47@gmail.com>
2022-11-16 16:43:09 +0530

    changes

---
+++ Express/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next(); //Allows the request to continue to the next middleware in line
+});
+app.use((req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1>Hello from express</h1>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
